2625e228290ee001a636fdfc017431333ba3b7ff
Lecodeas <edu.ianleu@gmail.com>
2026-01-22 17:37:41 -0600

    Librería de LCD
    INFO:
    - PD4 a PD7 es el Low Nibble de la data
    - PB0 a PB3 es el High Nibble de la data
    - PD2 es Modo de operación, PD3 es Enable
    - Rx y Tx reservados para serial
    - PC0 y PC1 leen los potenciómetros
    
    UPDATES:
    -  Se creó, se agregó el archivo para cálculo de manejo USART para la siguiente parte del Lab
    
    BUGS:
    - N/a (osea no lo he probado)
    
    NOTAS:
    -

---
+++ lAB2/lAB2/main.c
@@ -0,0 +1,28 @@
+/* -----------------------------------------------
+ Universidad del Valle de Guatemala
+ IE3054: Electrónica Digital 2
+ main.c
+ Autor: Ian Anleu Rivera
+ Proyecto: Nombre
+ Hardware: ATMEGA328P
+ Creado: 22/01/2026
+ Ultima modificacion: 22/01/2026
+    -----------------------------------------------
+*/
+
+//HEADER FILES
+#define F_CPU 160000000 //F cpu en 16 Mhz
+#include <avr/io.h> // IO regs
+#include <util/delay.h> //Delays
+#include <stdint.h> //Lib para enteros
+#include <avr/interrupt.h> //Lib para interrupciones
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
